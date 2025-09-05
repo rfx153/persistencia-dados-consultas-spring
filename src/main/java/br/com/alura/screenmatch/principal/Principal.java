41d5e4d38d827638a9c2ffc25392b29afbcd6e2e
Rafael Cardoso <rafaelfx153@gmail.com>
2025-09-05 09:53:47 -0300

    finalizando a aula 04 parte 05

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -47,6 +47,8 @@ public class Principal {
                 1 - Buscar séries
                 2 - Buscar episódios
                 3 - Listar séries buscadas
+                4 - Buscar série por titulo
+                5 - Buscar séries por ator 
                 0 - Sair                                 
                 """;
 
@@ -66,6 +68,12 @@ public class Principal {
                 break;
             case 3:
                 listarSeriesBuscadas();
+                break;
+            case 4:
+                buscarSeriesPorTitulo();
+                break;
+            case 5:
+                buscarSeriesPorAtor();
                 break;
             case 0:
                 System.out.println("Saindo...");
@@ -138,6 +146,33 @@ public class Principal {
         .sorted(Comparator.comparing(Serie::getGenero))
         .forEach(System.out::println);
     };
+
+    private void buscarSeriesPorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var nomeSerie = leitura.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if (serieBuscada.isPresent()) {
+            System.out.println("Dados da série: " + serieBuscada.get());
+        } else {
+            System.out.println("Serie não encontrada");
+        }
+        
+    }
+
+      private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor: ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        System.out.println(seriesEncontradas);
+        seriesEncontradas.forEach(s -> 
+        System.out.println(s.getTitulo() + "avalicao:  " + s.getAvaliacao()));
+      
+    }
+
 
     //private void buscar
 
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,10 +1,15 @@
 package br.com.alura.screenmatch.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.screenmatch.model.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, Long>{
-    
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);  
     
 }
