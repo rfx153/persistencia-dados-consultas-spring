2d7d05524bf6a57330511e000b8247a352c21a9e
Rafael Cardoso <rafaelfx153@gmail.com>
2025-09-23 09:20:25 -0300

    finalizando aula 05 parte 07

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -31,6 +31,8 @@ public class Principal {
 
     private List<Serie> series = new ArrayList<>();
 
+    private Optional<Serie> serieBusca;
+
     
 
     public Principal(SerieRepository repositorio) {
@@ -50,6 +52,7 @@ public class Principal {
                 7 - Buscar séries por categoria
                 8 - Buscar séries por avaliação e total de temporadas
                 9 - Buscar episódios por trecho
+                10 - Buscar 
                 0 - Sair                                 
                 """;
 
@@ -88,6 +91,9 @@ public class Principal {
             case 9: 
                 buscarEpisodioPorTrecho();  
                 break;  
+            case 10: 
+                topEpisodiosPorSerie();
+                break;
             case 0:
                 System.out.println("Saindo...");
                 break;
@@ -97,8 +103,6 @@ public class Principal {
         }
     }
 
-
-    
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
         Serie serie = new Serie(dados);
@@ -165,10 +169,10 @@ public class Principal {
     private void buscarSeriesPorTitulo() {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = leitura.nextLine();
-        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+        serieBusca = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
-        if (serieBuscada.isPresent()) {
-            System.out.println("Dados da série: " + serieBuscada.get());
+        if (serieBusca.isPresent()) {
+            System.out.println("Dados da série: " + serieBusca.get());
         } else {
             System.out.println("Serie não encontrada");
         }
@@ -230,4 +234,19 @@ public class Principal {
                                         e.getNumeroEpisodio(), e.getTitulo()));
        
     }
+
+     private void topEpisodiosPorSerie() {
+        buscarSeriesPorTitulo();
+        if(serieBusca.isPresent()){
+                Serie serie = serieBusca.get();
+                List<Episodio> topEpisodios = repositorio.topEpisodiosPorSerie(serie);
+                topEpisodios.forEach(e ->
+                                System.out.printf("Série: %s Temporada %s - Episódio %s - %s Avaliação %s\n",
+                                                e.getSerie().getTitulo(), e.getTemporada(),
+                                                e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao() ));
+        }
+        
+    }
+
+    
 }
