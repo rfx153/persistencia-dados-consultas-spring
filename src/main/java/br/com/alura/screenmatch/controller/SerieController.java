8c4467bd33c78bee6408dba28f6ac622275e96df
Rafael Cardoso <rafaelfx153@gmail.com>
2025-11-04 12:12:06 -0300

    finalizando parte 03

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,34 @@
 package br.com.alura.screenmatch.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repositorio;
 
     @GetMapping("/series")
-    public String obterSeries() {
-        return "aqui serão listadas as séries";
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    // testando se está funcionando
+    @GetMapping("/inicio")
+    public String retornarInicio() {
+        return "Bem-vindo ao Screenmatch!";
     }
 
 }
